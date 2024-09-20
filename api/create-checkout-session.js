5e7f1be579514da45b8816b12919c40eca73f40d
limousineairline <limousineairline@gmail.com>
2024-09-20 05:59:21 -0400

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,26 +1,10 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);  // Replace with your secret key
 const express = require('express');
 const app = express();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const cors = require('cors');
-
-// Define allowed origins for CORS
-const allowedOrigins = ['http://127.0.0.1:5500', 'https://airlinelimousines.com'];
-
-// Middleware to set CORS headers
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 
 app.use(express.json());
 
-app.post('/create-payment-intent', async (req, res) => {
+app.post('/api/create-checkout-session', async (req, res) => {
     try {
         const { amount } = req.body;
 
@@ -35,6 +19,4 @@ app.post('/create-payment-intent', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+module.exports = app;
